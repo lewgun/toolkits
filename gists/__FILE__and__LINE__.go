bd4dd9f9b6ffbabcae89863edcc5d332b9ce6993
liugang <lewgun@gmail.com>
2015-03-11 10:12:46 +0800

    example like C's __FILE__ & __LINE__

---
+++ gists/__FILE__and__LINE__.go
@@ -0,0 +1,15 @@
+
+//http://golangtc.com/t/54f7d5d0421aa9089a000047
+package main
+
+import "fmt"
+import "runtime"
+
+func main() {
+
+    pc, file, line, ok := runtime.Caller(0)
+    if ok {
+        fmt.Println("Func Name = ", runtime.FuncForPC(pc).Name())
+        fmt.Printf("file: %s line=%d\n", file, line )
+    }
+}
