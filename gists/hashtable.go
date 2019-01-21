a42eb5771642a4f89f71942747d0195c1c6e6bae
lewgun <lewgun@gmail.com>
2019-01-21 17:08:09 +0800

    Update hashtable.go

--- gists/hashtable.go
+++ gists/hashtable.go
@@ -2,172 +2,200 @@ package main
 
 import "fmt"
 
-var (
-	minLoadFactor = 0.25
-	maxLoadFactor = 0.75
-	defaultTableSize = 3
+const (
+	maxBuckets   = 30
+	minBuckets   = 10
+	expandFactor = 0.75
+	shrinkFactor = 0.25
 )
 
-type Record struct {
-	key int
-	value int
-	next *Record
+type node struct {
+	k    int
+	v    int
+	next *node
 }
 
-type Hash struct {
-	records []*Record
+type hashTable struct {
+	buckets []node
+
+	bucketNum int
+
+	count int
 }
 
-type HashTable struct {
-	table *Hash
-	nRecords *int
-}
-
-// createHashTable: Called by checkLoadFactorAndUpdate when creating a new hash, for internal use only.
-func createHashTable(tableSize int) HashTable {
-	num := 0
-	hash := Hash{make([]*Record, tableSize)}
-	return HashTable{table: &hash, nRecords:&num}
-}
-
-// CreateHashTable: Called by the user to create a hashtable.
-func CreateHashTable() HashTable {
-	num := 0
-	hash := Hash{make([]*Record, defaultTableSize)}
-	return HashTable{table: &hash, nRecords:&num}
-}
-
-// hashFunction: Used to calculate the index of record within the slice
-func hashFunction(key int, size int) (int) {
-	return key%size
-}
-
-// Display: Print the hashtable in a legible format (publicly callable)
-func (h *HashTable) Display() {
-	fmt.Printf("----------%d elements-------\n", *h.nRecords)
-	for i, node := range h.table.records {
-		fmt.Printf("%d :", i)
-		for node != nil {
-			fmt.Printf("[%d, %d]->", node.key, node.value)
-			node = node.next
-		}
-		fmt.Println("nil")
+func newHashTableHelper(buckets int) *hashTable {
+	return &hashTable{
+		bucketNum: buckets,
+		buckets:   make([]node, buckets),
 	}
 }
-
-// put: inserts a key into the hash table, for internal use only
-func (h *HashTable) put(key int, value int) (bool){
-	index := hashFunction(key, len(h.table.records))
-	iterator := h.table.records[index]
-	node := Record{key, value, nil}
-	if iterator == nil {
-		h.table.records[index] = &node
-	} else {
-		prev := &Record{0, 0, nil}
-		for iterator != nil {
-			if iterator.key == key { // Key already exists
-				iterator.value = value
-				return false
-			}
-			prev = iterator
-			iterator = iterator.next
-		}
-		prev.next = &node
-	}
-	*h.nRecords += 1
-	return true
+func newHashTable() *hashTable {
+	return newHashTableHelper(minBuckets)
 }
 
-// Put: inserts a key into the hash table (publicly callable)
-func (h *HashTable) Put(key int, value int) {
-	sizeChanged := h.put(key, value)
-	if sizeChanged == true {
-		h.checkLoadFactorAndUpdate()
-	}
+func (h *hashTable) hash(k int) int {
+	return k % h.bucketNum
 }
 
-// Get: Retrieve a value for a key from the hash table (publicly callable)
-func (h *HashTable) Get(key int) (bool, int) {
-	index := hashFunction(key, len(h.table.records))
-	iterator := h.table.records[index]
-	for iterator != nil {
-		if iterator.key == key {	// Key already exists
-			return true, iterator.value
-		}
-		iterator = iterator.next
-	}
-	return false, 0
-}
-
-// del: remove a key-value record from the hash table, for internal use only
-func (h *HashTable) del(key int) (bool) {
-	index := hashFunction(key, len(h.table.records))
-	iterator := h.table.records[index]
-	if iterator == nil {
-		return false
-	}
-	if iterator.key == key {
-		h.table.records[index] = iterator.next
-		*h.nRecords--
-		return true
-	} else {
-		prev := iterator
-		iterator = iterator.next
-		for iterator != nil {
-			if iterator.key == key {
-				prev.next = iterator.next
-				*h.nRecords--
-				return true
-			}
-			prev = iterator
-			iterator = iterator.next
-		}
-		return false
-	}
-}
-
-// Del: remove a key-value record from the hash table (publicly available)
-func (h *HashTable) Del(key int) (bool) {
-	sizeChanged := h.del(key)
-	if sizeChanged == true {
-		h.checkLoadFactorAndUpdate()
-	}
-	return sizeChanged
-}
-
-// getLoadFactor: calculate the loadfactor for the hashtable
-// Calculated as: number of records stored / length of underlying slice used
-func (h *HashTable) getLoadFactor() (float64) {
-	return float64(*h.nRecords)/float64(len(h.table.records))
-}
-
-// checkLoadFactorAndUpdate: if 0.25 > loadfactor or 0.75 < loadfactor,
-// update the underlying slice to have have loadfactor close to 0.5
-func (h *HashTable) checkLoadFactorAndUpdate() {
-	if *h.nRecords == 0 {
+func (h *hashTable) put(k, v int) {
+	ok := h.putHelper(k, v)
+	if !ok {
 		return
-	} else {
-		loadFactor := h.getLoadFactor()
-		if loadFactor < minLoadFactor {
-			fmt.Println("** Loadfactor below limit, reducing hashtable size **")
-			hash := createHashTable(len(h.table.records)/2)
-			for _, record := range h.table.records {
-				for record != nil {
-					hash.put(record.key, record.value)
-					record = record.next
-				}
-			}
-			h.table = hash.table
-		} else if loadFactor > maxLoadFactor {
-			fmt.Println("** Loadfactor above limit, increasing hashtable size **")
-			hash := createHashTable(*h.nRecords*2)
-			for _, record := range h.table.records {
-				for record != nil {
-					hash.put(record.key, record.value)
-					record = record.next
-				}
-			}
-			h.table = hash.table
+	}
+
+	 h.adjust(true)
+}
+func (h *hashTable) putHelper(k, v int) bool {
+
+	index := h.hash(k)
+
+	n := &node{
+		k: k,
+		v: v,
+	}
+	if h.buckets[index].next == nil {
+		h.buckets[index].next = n
+		h.count++
+		return true
+
+	}
+
+	p := h.buckets[index].next
+
+	//update
+	for p != nil {
+		if p.k == k {
+			p.v = v
+			return false
 		}
 	}
+	n.next = h.buckets[index].next
+	h.buckets[index].next = n
+	h.count++
+
+	return true
+
+}
+
+func (h *hashTable) adjust(isExpand bool)  {
+	factor := float32(h.count) / float32(h.bucketNum)
+
+	num := h.bucketNum
+	if isExpand {
+		// do nothing
+		if factor <= expandFactor || h.bucketNum == maxBuckets {
+			return
+		}
+
+		num *= 2
+		if num > maxBuckets {
+			num = maxBuckets
+		}
+		fmt.Println("++++expand the  hash table", num)
+
+	} else {
+		// do nothing
+		if factor >= shrinkFactor || h.bucketNum == minBuckets {
+			return
+		}
+
+		num /= 2
+		if num < minBuckets {
+			num = minBuckets
+		}
+
+		fmt.Println("----shrink the hash table", num)
+	}
+
+
+	ht := newHashTableHelper(num)
+
+	for _, bucket := range h.buckets {
+		for p := bucket.next; p != nil; p = p.next {
+			ht.put(p.k, p.v)
+		}
+	}
+
+	h.bucketNum = ht.bucketNum
+	h.buckets = ht.buckets
+	h.count = ht.count
+
+}
+
+func (h *hashTable) get(k int) (int, bool) {
+	index := h.hash(k)
+	if h.buckets[index].next == nil {
+		return 0, false
+	}
+
+	p := h.buckets[index].next
+
+	//update
+	for p != nil {
+		if p.k == k {
+			return p.v, true
+		}
+	}
+
+	return 0, false
+
+}
+
+func (h *hashTable) print() {
+	for _, bucket := range h.buckets {
+		for p := bucket.next; p != nil; p = p.next {
+			fmt.Println(p.k, " => ", p.v)
+		}
+	}
+
+	fmt.Println("count", h.count, "size:", h.bucketNum)
+}
+
+func (h *hashTable)delete(k int) {
+
+	ok := h.deleteHelper(k)
+	if !ok {
+		return
+	}
+	h.adjust(false )
+}
+func (h *hashTable) deleteHelper(k int) bool  {
+	index := h.hash(k)
+	if h.buckets[index].next == nil {
+		return  false
+	}
+
+	prev := &h.buckets[index]
+	for p := prev.next; p != nil; p = p.next {
+		if p.k == k {
+				prev.next = p.next
+				h.count--
+				return true
+		}
+		prev = p
+	}
+
+	return false
+}
+
+func (h *hashTable) size() int  {
+	return h.count
+}
+
+func main() {
+	h := newHashTable()
+
+	for i := 0; i < 10; i++ {
+		h.put(i, i*10)
+	}
+	//h.print()
+
+	fmt.Println(h.get(5))
+
+	for i := 0; i < 10; i++ {
+		h.delete(i)
+	}
+	h.print()
+
+	fmt.Println(h.get(5))
 }
